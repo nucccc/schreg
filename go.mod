cf051405b04b2438184aa39e164d607e42d020d5
Nuc94 <dan.dom.nuc@gmail.com>
2023-01-07 08:31:14 +0100

    added testcontainers in testing

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/hamba/avro v1.1.1
-	github.com/kr/pretty v0.1.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	github.com/testcontainers/testcontainers-go v0.17.0
 )
+
+replace github.com/docker/docker => github.com/docker/docker v20.10.3-0.20221013203545-33ab36d6b304+incompatible // 22.06 branch
--- schreg_test.go
+++ schreg_test.go
@@ -1,8 +1,11 @@
 package schreg
 
 import (
+	"context"
 	"fmt"
 	"testing"
+
+	testcontainers "github.com/testcontainers/testcontainers-go"
 )
 
 func TestDefault(t *testing.T) {
@@ -18,4 +21,25 @@ func TestDefault(t *testing.T) {
 
 func TestConstructor(t *testing.T) {
 	_, _ = NewSchRegClient(default_config_map)
+}
+
+func TestTestContainer(t *testing.T) {
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:        "confluentinc/cp-schema-registry:5.4.1",
+		ExposedPorts: []string{"8081/tcp"},
+		//WaitingFor:   wait.ForLog("Ready to accept connections"),
+	}
+	schema_reg, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	defer func() {
+		if err := schema_reg.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %s", err.Error())
+		}
+	}()
 }
